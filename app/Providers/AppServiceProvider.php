42e3827c26e0732fad1083d7599a3b1cdfc213a7
krthush <33790334+krthush@users.noreply.github.com>
2017-12-15 02:29:25 +0000

    Removal of arraypush

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,7 +36,6 @@ class AppServiceProvider extends ServiceProvider
 
                 $selectUserTrees = \App\Tree::where('user_id',$userID)->pluck('title','id')->all();
                 $sharedTrees = \App\Tree::where('shared',true)->where('university','!=',true)->orderBy('likes','desc')->get();
-                $selectUserTrees = array_merge($selectUserTrees, $sharedTrees);
 
                 $view->with('selectUserTrees',$sharedTrees);
                 $view->with('sharedTrees', $sharedTrees);
