daf727ad1ef63e53de3f3fecad6b514114ad9410
krthush <33790334+krthush@users.noreply.github.com>
2018-01-23 13:19:37 +0000

    optimization

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,70 +4,66 @@
 </footer>
 </div>
 
+<!-- Bootstrap -->
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-	<!-- Bootstrap -->
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-
-
-  	<!-- Custom JS -->
-  	<script src="/js/custom.js"></script>
-  	<script>
-  		$(function() {
-  			
-			$('[data-toggle="tooltip"]').tooltip();
+<!-- Custom JS -->
+<script src="/js/custom.js"></script>
+<script>
+	$(function() {
 			
-	        $('#showMoreButton1').click( function() {
-	            $("#filteredSharedTrees").hide();
-	            $("#sharedTrees").show();
-	            $("#showMoreContainer1").hide();
-	            $("#showLessContainer1").show();
-	        });
-	    	$('#showLessButton1').click( function() {
-	            $("#filteredSharedTrees").show();
-	            $("#sharedTrees").hide();
-	            $("#showMoreContainer1").show();
-	            $("#showLessContainer1").hide();
-	        });
-	        $('#showMoreButton2').click( function() {
-	            $("#filteredSharedTrees").hide();
-	            $("#sharedTrees").show();
-	            $("#showMoreContainer2").hide();
-	            $("#showLessContainer2").show();
-	        });
-	    	$('#showLessButton2').click( function() {
-	            $("#filteredSharedTrees").show();
-	            $("#sharedTrees").hide();
-	            $("#showMoreContainer2").show();
-	            $("#showLessContainer2").hide();
-	        });
-	    	$('#showAllButton1').click( function() {
-	    		window.location.replace("http://homestead.app/trees")
-	        });
-	    	$('#showAllButton2').click( function() {
-	    		window.location.replace("http://homestead.app/trees")
-	        });		        
+		$('[data-toggle="tooltip"]').tooltip();
+		
+	    $('#showMoreButton1').click( function() {
+	        $("#filteredSharedTrees").hide();
+	        $("#sharedTrees").show();
+	        $("#showMoreContainer1").hide();
+	        $("#showLessContainer1").show();
 	    });
-  	</script>
-  	<script type="text/javascript">
-	  function submitForm(action) {
-	    var form = document.getElementById('addTreeForm');
-	    form.action = action;
-	    form.submit();
-	  }
-	</script>
-	<script>
-  		$(window).scroll(function() {
-		  sessionStorage.scrollTop = $(this).scrollTop();
-		});
+		$('#showLessButton1').click( function() {
+	        $("#filteredSharedTrees").show();
+	        $("#sharedTrees").hide();
+	        $("#showMoreContainer1").show();
+	        $("#showLessContainer1").hide();
+	    });
+	    $('#showMoreButton2').click( function() {
+	        $("#filteredSharedTrees").hide();
+	        $("#sharedTrees").show();
+	        $("#showMoreContainer2").hide();
+	        $("#showLessContainer2").show();
+	    });
+		$('#showLessButton2').click( function() {
+	        $("#filteredSharedTrees").show();
+	        $("#sharedTrees").hide();
+	        $("#showMoreContainer2").show();
+	        $("#showLessContainer2").hide();
+	    });
+		$('#showAllButton1').click( function() {
+			window.location.replace("http://homestead.app/trees")
+	    });
+		$('#showAllButton2').click( function() {
+			window.location.replace("http://homestead.app/trees")
+	    });
 
-		$(document).ready(function() {
-		  if (sessionStorage.scrollTop != "undefined") {
-		    $(window).scrollTop(sessionStorage.scrollTop);
-		  }
-		});
-	</script>
+		if (sessionStorage.scrollTop != "undefined") {
+			$(window).scrollTop(sessionStorage.scrollTop);
+		}
+
+	});
+</script>
+<script type="text/javascript">
+	function submitForm(action) {
+		var form = document.getElementById('addTreeForm');
+		form.action = action;
+		form.submit();
+	}
+</script>
+<script>
+	$(window).scroll(function() {
+		sessionStorage.scrollTop = $(this).scrollTop();
+	});
+</script>
 @isset($infoContents)
 <script>     
 	$( window ).on( "load", function() {
