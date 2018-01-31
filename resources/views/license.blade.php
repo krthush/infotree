8bab3bd0b321f65ba9e9a9e5ce62afcc93cef209
krthush <33790334+krthush@users.noreply.github.com>
2018-01-31 23:12:20 +0000

    license changes

--- resources/views/license.blade.php
+++ resources/views/license.blade.php
@@ -4,7 +4,7 @@
 
 <div class="midContainer">
     <div class="midContainerHeader">
-        <div class="midContainerHeaderText"><b>Terms and Conditions</b></div>
+        <div class="midContainerHeaderText"><b>Licenses & Accreditations</b></div>
     </div>
     <div class="midContainerContent">
 
@@ -121,6 +121,23 @@
 			nasrulhazim's activation code
 			<br>
 			<a href="https://gist.github.com/nasrulhazim/fc363bdf9044f08a2a39556648f1fdce">github</a>
+			<br>
+			<br>
+			Masonry
+			<br>
+			The MIT License (MIT)
+			<br>
+			<a href="https://desandro.mit-license.org/">License</a>
+			<br>
+			<a href="https://github.com/desandro/masonry">github</a>
+			<br>
+			Copyright © 2018 David DeSandro
+			<br>
+			Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+			<br>
+			The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+			<br>
+			THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 			<br>
 			<br>
 			Laracasts - main tool for learn laravel framework.
